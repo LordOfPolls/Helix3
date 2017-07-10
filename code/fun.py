b46cf3c17afb7300d7a72725e70650c59a1e67ad
Joel <joeljroy123@gmail.com>
2017-07-10 21:51:01 +0100

    Update fun.py

--- code/fun.py
+++ code/fun.py
@@ -6,7 +6,8 @@ class Fun:
     def __init__(self, bot):
         self.bot = bot
         
-          @commands.command(pass_context=True, no_pm=True)
-    async def ping(message):
-          if message.content.startswith('!ping'):
-       await client.send_message(message.channel, 'Pong!')
+ @commands.command(pass_context=True, no_pm=True)
+ async def ping(message):
+     if message.content.startswith('!ping'):
+         return await my_bot.say("Hello, world!")
+
