894126e3442d6d80227294d36177069cd342f220
Joel <joeljroy123@gmail.com>
2017-07-10 21:55:29 +0100

    Update fun.py

--- code/fun.py
+++ code/fun.py
@@ -5,8 +5,8 @@ from discord.ext import commands
 class Fun:
     def __init__(self, bot):
         self.bot = bot
-        
- @commands.command(pass_context=True, no_pm=True)
- async def ping(self,ctx):
- """A Ping command made by panic"""
-         await self.bot.send_message(ctx.message.channel, "Pong!")
+       
+    @commands.command(pass_context=True)
+    async def panicping(self, ctx, *args):
+        """Panic's dead ass ping command"""
+        await self.bot.say('Pong')
