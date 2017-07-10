10dbb53210ac54231d4596920fe5175b83082d1d
Joel <joeljroy123@gmail.com>
2017-07-10 21:41:59 +0100

    Update fun.py

--- code/fun.py
+++ code/fun.py
@@ -5,3 +5,8 @@ from discord.ext import commands
 class Fun:
     def __init__(self, bot):
         self.bot = bot
+        
+          @commands.command(pass_context=True, no_pm=True)
+    async def ping(message):
+          if message.content.startswith('!ping'):
+       await client.send_message(message.channel, 'Pong!')
