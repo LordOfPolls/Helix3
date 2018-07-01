140fa09095cc84eb4459bf0efdf121463a25eaeb
LordOfPolls <22540825+LordOfPolls@users.noreply.github.com>
2018-07-01 13:01:44 +0100

    removed legacy command

--- code/utilities.py
+++ code/utilities.py
@@ -107,48 +107,6 @@ class Utilities:
         await self.bot.edit_message(msg, "Pong!\n\nPing: {}ms".format(str(elapsed)))
 
     @commands.command(pass_context=True, no_pm=True)
-    async def prefix(self, ctx):
-        """Change the bots prefix for your server"""
-        message = ctx.message.content.strip()
-        message = message.replace("<@{}> ".format(str(self.bot.user.id)), "")
-        length = 6 + int(len(await get.Prefix(ctx.message.server)))
-        if len(message) == length:
-            message = None
-        else:
-            message = message.split(" ")
-            try:
-                message = message[1]
-            except IndexError:
-                message = None
-        server = ctx.message.server
-        channel = ctx.message.channel
-
-        dir = "data/" + server.id + ".json"
-        if not os.path.exists("data"):
-            os.mkdir("data")
-        if not os.path.isfile(dir):
-            with open(dir, 'w') as r:
-                entry = {'welcome': channel.id, 'prefix': ".", 'urlblock': 'True', 'mention': 'False',
-                         'announcement': 'None', 'modrole': 'None', 'modlog': 'None'}
-                json.dump(entry, r)
-        else:
-            with open(dir, 'r+') as r:
-                data = json.load(r)
-                try:
-                    if message == None:
-                        await self.bot.send_message(channel,
-                                                    "My current prefix here is ``{}``\n".format(data['prefix']))
-                        return
-                    data['prefix'] = str(message)
-                except:
-                    await self.bot.send_message(channel,"My current prefix here is ``{}``\n".format(data['prefix']))
-                    return
-                r.seek(0)
-                r.write(json.dumps(data))
-                r.truncate()
-        await self.bot.send_message(channel, "Set prefix to ``" + str(message) + "``")
-
-    @commands.command(pass_context=True, no_pm=True)
     async def server(self, ctx):
         """Shows the server's information"""
         channel = ctx.message.channel
