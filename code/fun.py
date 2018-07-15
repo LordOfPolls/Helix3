fc136b25d4e069cbe2061c378d0372af97f1cf0f
LordOfPolls <ddavidallen13@gmail.com>
2018-07-15 20:22:54 +0100

    Removed retarded commands

--- code/fun.py
+++ code/fun.py
@@ -47,28 +47,6 @@ class Fun:
         await self.bot.delete_message(ctx.message)
 
     @commands.command(pass_context = True)
-    async def fakekick(self, ctx):
-        """Fake kicks a user"""
-        await self.bot.send_typing(ctx.message.channel)
-        await self.bot.delete_message(ctx.message)
-        user_mentions = list(map(ctx.message.server.get_member, ctx.message.raw_mentions))
-        for user in user_mentions:
-            await self.bot.say("<@{}>, i kicked <@{}>.".format(ctx.message.author.id, user.id))
-        if not user_mentions:
-            await self.bot.say("<@{}>, Invalid user.".format(ctx.message.author.id))
-
-    @commands.command(pass_context = True)
-    async def fakeban(self, ctx):
-        """Fake bans a user"""
-        await self.bot.send_typing(ctx.message.channel)
-        await self.bot.delete_message(ctx.message)
-        user_mentions = list(map(ctx.message.server.get_member, ctx.message.raw_mentions))
-        for user in user_mentions:
-            await self.bot.say("<@{}>, i banned <@{}>.".format(ctx.message.author.id, user.id))
-        if not user_mentions:
-            await self.bot.say("<@{}>, Invalid user.".format(ctx.message.author.id))
-
-    @commands.command(pass_context = True)
     async def dog(self):
         """Sends a dog pic"""
         def findDog():
